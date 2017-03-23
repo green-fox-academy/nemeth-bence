f46a19c1627d688407625bf33a6e31c116152a2d
nemeth-bence <nemeth.bence@alwt.hu>
2017-03-23 11:40:32 +0100

    Examples

---
+++ Week-02/Day-04/sum-function.py
@@ -0,0 +1,9 @@
+number = 5
+
+def sum(n):
+    out = 0
+    for i in range(1, n+ 1):
+        out += i
+    return out
+
+print(sum(number))
---
+++ Week-02/Day-04/sum.py
@@ -0,0 +1,8 @@
+number = 5
+
+out = 0
+
+for i in range(1, number + 1):
+    out += i
+
+print(out)

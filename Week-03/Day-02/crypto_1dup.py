024d8997d85832a960396934ad91cbbc3428c7b7
nemeth-bence <nemeth.bence@alwt.hu>
2017-03-28 11:33:05 +0200

    Decrypting - exercise - 01

---
+++ Week-03/Day-02/crypto_1dup.py
@@ -0,0 +1,13 @@
+# Create a method that decrypts the texts/duplicated_chars.txt
+
+fr = open("duplicated-chars.txt", "r")
+text = fr.read()
+
+def decrypt(duplicated_chars):
+    text_decrypt=""
+    for i in range(0, len(duplicated_chars), 2):
+        text_decrypt += duplicated_chars[i]
+
+    return text_decrypt
+
+print(decrypt(text))

b3fff03ea9fab11d3941b2baae388e603c6ab06c
nemeth-bence <nemeth.bence@alwt.hu>
2017-03-31 13:59:28 +0200

    Try-angle

---
+++ Week-03/Day-05/triangle.py
@@ -0,0 +1,23 @@
+from tkinter import *
+import time
+
+root = Tk()
+canvas = Canvas(root, width='400', height='400')
+canvas.pack()
+
+def draw_triangle(x, y, size):
+    triangle = canvas.create_polygon(x, y, x + size, y, (x + size)/2, y + size, fill= "white", outline="black", width=2)
+
+def recursive_something(x, y, size):
+    draw_triangle(x, y, size)
+    time.sleep(0.1)
+    canvas.update()
+    if size <= 10:
+        recursive_something(x/2, y, size/3)
+        recursive_something(x+2/3*size, y, size/3)
+        recursive_something(x, y+2/3*size, size/3)
+        recursive_something(x+2/3*size, y+2/3*size, size/3)
+
+draw_triangle(10, 10, 380)
+
+root.mainloop()

8ddd5ba13496f5038cce32cf4bd265ae330a29db
nemeth-bence <nemeth.bence@alwt.hu>
2017-04-13 18:28:25 +0200

    Wanderer - hero positioned

--- Week-05/Day-02/python-main.py
+++ Week-05/Day-02/python-main.py
@@ -4,15 +4,8 @@ class GameObject():
 
     def __init__(self):
         self.image_size = 72
-        #self.character = character
-        #self.move = move
-        #self.position = position
         self.floor_image = PhotoImage(file = 'assets\Floor.png')
         self.wall_image = PhotoImage(file = 'assets\Wall.png')
-
-        # self.draw_image(2, 5, self.floor_image)
-        # self.draw_image(6, 4, self.wall_image)
-
 
     def draw_image(self, x, y, tile):
         id = canvas.create_image(x*self.image_size, y*self.image_size, anchor = NW, image = tile)
@@ -26,25 +19,23 @@ class Character():
         self.hero_y = 0
         self.hero = game.draw_image(self.hero_x, self.hero_y, self.hero_image)
 
-    def move(self):
-        canvas.move(self.hero, game.image_size, game.image_size)
+    def move(self, direction):
+        y = 0
+        x = 0
+        if direction == 'up':
+            y = y - game.image_size
+        if direction == 'right':
+            x = x + game.image_size
+        if direction == 'down':
+            y = y + game.image_size
+        if direction == 'left':
+            x = x - game.image_size
 
-        print('hello')
-        #self.hero_image = PhotoImage(file = 'assets\hero_down.png')
+        canvas.move(self.hero, x, y)
 
     # def get_position(self, x, y):
     #     self.position = self.canvas.create_image(x*image_size, y*image_size, image = self.photo)
 
-
-# class Wall(GameObject):
-#     def __init__(self,root,canvas,background="wall.gif"):
-#         super().__init__(root,canvas,background)
-#
-# class Floor(GameObject):
-#     def __init__(self,root,canvas,background="floor.gif"):
-#         super().__init__(root,canvas,background)
-#
-#
 class GameMap:
 
     def __init__(self):
@@ -70,18 +61,6 @@ class GameMap:
                     game.draw_image(j, i, game.wall_image)
 
 
-
-
-
-    # def crt_objts_map(self):
-    #     for i in range(len(self.game_area)):
-    #         for j in range(len(self.game_area[i])):
-    #             if self.game_area[i][j] == 1:
-    #                 self.wall.create_object(i*72,j*72)
-    #             if self.game_area[i][j] == 0:
-    #                 self.floor.create_object(i*72,j*72)
-#
-#
 # class Gameplay():
 #         pass
 
@@ -93,7 +72,7 @@ game = GameObject()
 map = GameMap ()
 map.create_map()
 hero = Character()
-hero.move()
+#hero.move()
 
 canvas.pack()
 

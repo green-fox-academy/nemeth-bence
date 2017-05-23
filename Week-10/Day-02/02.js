bd67d1baa73af9aa5100915072c32285f4cde551
nemeth-bence <nemeth.bence@alwt.hu>
2017-05-23 13:27:44 +0200

    02.js solved

---
+++ Week-10/Day-02/02.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// Create a constructor for creating Rectangles.
+// it should take two parameters: the sides of the rectangle
+// Every rectangle should have a method called getArea() that returns its area
+// Every rectangle should have a method called getCircumference() that returns its circumference
+
+function Rectangles(aSide, bSide) {
+    this.getArea = function () {
+        console.log(a * b);
+    };
+    this.getCircumference = function () {
+        console.log(2 * (a + b));
+    };
+}
+
+var rectangle1 = new Rectangles(5,5);
+rectangle1.getArea();
+rectangle1.getCircumference();
+
+var rectangle2 = new Rectangles(10,20);
+
+rectangle2.getArea();
+rectangle2.getCircumference();

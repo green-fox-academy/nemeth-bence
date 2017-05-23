5c5ae1cb54b3c5d21745de04db090ad33f93c431
nemeth-bence <nemeth.bence@alwt.hu>
2017-05-23 15:47:28 +0200

    TennisGame

--- Week-10/Day-01/TennisGame1.js
+++ Week-10/Day-01/TennisGame1.js
@@ -1,4 +1,4 @@
-var TennisGame1 = function(player1Name, player2Name) {
+var  TennisGame1 = function(player1Name, player2Name) {
     this.m_score1 = 0;
     this.m_score2 = 0;
     this.player1Name = player1Name;
@@ -6,9 +6,9 @@ var TennisGame1 = function(player1Name, player2Name) {
 };
 
 TennisGame1.prototype.wonPoint = function(playerName) {
-    if (playerName === "player1")
+    if (playerName === "player1"){
         this.m_score1 += 1;
-    else
+    } else
         this.m_score2 += 1;
 };
 
@@ -32,14 +32,18 @@ TennisGame1.prototype.getScore = function() {
         }
     } else if (this.m_score1 >= 4 || this.m_score2 >= 4) {
         var minusResult = this.m_score1 - this.m_score2;
-        if (minusResult === 1) score = "Advantage player1";
-        else if (minusResult === -1) score = "Advantage player2";
-        else if (minusResult >= 2) score = "Win for player1";
-        else score = "Win for player2";
+        if (minusResult === 1){
+           score = "Advantage player1";
+        } else if (minusResult === -1) {
+          score = "Advantage player2";
+        } else if (minusResult >= 2) {
+          score = "Win for player1";
+        } else score = "Win for player2";
     } else {
         for (var i = 1; i < 3; i++) {
-            if (i === 1) tempScore = this.m_score1;
-            else {
+            if (i === 1) {
+              tempScore = this.m_score1;
+          } else {
                 score += "-";
                 tempScore = this.m_score2;
             }
